c7f008e3b596536fabc76999c3112ed66d32a13b
Dr. Sudip Chakraborty <sudip.embedded@gmail.com>
2024-03-02 19:34:10 +0530

    Initial commit

---
+++ LowBattery_WaterLevel_Notification/LowBattery_WaterLevel_Notification.ino
@@ -0,0 +1,70 @@
+/* 
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-send-messages-whatsapp/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <UrlEncode.h>
+
+const char* ssid = "Dr.SudipChakraborty";
+const char* password = "abcd@9876543210";
+
+// +international_country_code + phone number
+// Portugal +351, example: +351912345678
+String phoneNumber = "+917003034313";
+String apiKey = "mDnEtehKtP7H";
+
+void sendMessage(String message){
+
+  // Data to send with HTTP POST http://api.callmebot.com/whatsapp.php?phone=
+  String url = "http://api.textmebot.com/send.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);
+  WiFiClient client;    
+  HTTPClient http;
+  http.begin(client, url);
+
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(url);
+  if (httpResponseCode == 200){
+    Serial.print("Message sent successfully");
+  }
+  else{
+    Serial.println("Error sending the message");
+    Serial.print("HTTP response code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Free resources
+  http.end();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Send Message to WhatsAPP
+  sendMessage("Hello from ESP8266!");
+}
+
+void loop() {
+  
+}
