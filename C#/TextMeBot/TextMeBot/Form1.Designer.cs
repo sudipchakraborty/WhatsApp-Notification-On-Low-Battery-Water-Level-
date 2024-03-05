fb6eebc6c403874240bc85c47b3f6d186588de24
MSI\sudip <sudip.embedded@gmail.com>
2024-03-05 23:38:50 +0530

    C# simulation Added

---
+++ C#/TextMeBot/TextMeBot/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace TextMeBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_send = new System.Windows.Forms.Button();
+            this.txt_API_Key = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_ph_number = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(135, 145);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(137, 29);
+            this.btn_send.TabIndex = 0;
+            this.btn_send.Text = "SEND";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // txt_API_Key
+            // 
+            this.txt_API_Key.Location = new System.Drawing.Point(115, 74);
+            this.txt_API_Key.Name = "txt_API_Key";
+            this.txt_API_Key.Size = new System.Drawing.Size(172, 20);
+            this.txt_API_Key.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(31, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "API Key";
+            // 
+            // txt_ph_number
+            // 
+            this.txt_ph_number.Location = new System.Drawing.Point(115, 100);
+            this.txt_ph_number.Name = "txt_ph_number";
+            this.txt_ph_number.Size = new System.Drawing.Size(172, 20);
+            this.txt_ph_number.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(31, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Phone Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(201, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(218, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TextMeBot Test Interface";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(661, 466);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_ph_number);
+            this.Controls.Add(this.txt_API_Key);
+            this.Controls.Add(this.btn_send);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.TextBox txt_API_Key;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_ph_number;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ C#/TextMeBot/TextMeBot/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace TextMeBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        static async Task send()
+        {
+            // Create HttpClient instance
+            using (HttpClient httpClient = new HttpClient())
+            {
+                // Define the URL you want to send the request to
+                string requestUrl = "http://api.textmebot.com/send.php?phone=+917003034313&apikey=jXYcsR1XTdW2&text=Test Message_05.03.2024";
+
+                // Send GET request
+                HttpResponseMessage response = await httpClient.GetAsync(requestUrl);
+
+                // Check if the response is successful
+                if (response.IsSuccessStatusCode)
+                {
+                    // Read and display the response content
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseBody);
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode}");
+                }
+            }
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            send();
+        }
+    }
+}
